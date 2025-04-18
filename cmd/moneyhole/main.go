f500ff7ad2a227d74b2282d3d19c72db85507e7f
Pushin Maxim <macsime12321@gmail.com>
2025-04-18 23:16:52 +0300

    version v2.0

--- cmd/moneyhole/main.go
+++ cmd/moneyhole/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"MoneyHole/internal/aggregator"
+	"context"
 	"errors"
 	"log/slog"
 	"net/http"
@@ -21,22 +22,223 @@ func main() {
 	if err != nil {
 		slog.Error(err.Error())
 	}
+	signalChan := make(chan struct{}, 1)
+
+	e.Use(func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			c.Set("signalChan", signalChan)
+			return next(c)
+		}
+	})
 
 	e.GET("/", hello)
 	e.POST("/users/add", add_user)
-	e.GET("/users/show", show_users)
+	e.POST("/headline/add", add_headline)
+	e.GET("/users/show", get_users)
+	e.GET("/users/quotes", get_quotes)
+	e.GET("/headlines", get_headlines)
 
 	go func() {
 		if err := e.Start(":8080"); err != nil && !errors.Is(err, http.ErrServerClosed) {
 			slog.Error("failed to start server", "error", err)
 		}
 	}()
+	
 	for {
 		go aggregator.Run()
-		time.Sleep(10 * time.Minute)
+		ctx, cancel := context.WithTimeout(context.Background(), time.Second * 10)
+		select {
+		case <- ctx.Done():
+		case <- signalChan:
+		}
+		cancel()
 	}
 }
 
+func get_headlines(c echo.Context) error {
+	db, err := sql.Open("sqlite3", "my.sql")
+	if err != nil {
+		return err
+	}
+	defer db.Close()
+
+
+	users_db, err := db.Query(
+		`select id, id_message from users`,
+	)
+	if err != nil {
+		return err
+	}
+	defer users_db.Close()
+	response := make([]UserHeadline, 0)
+	for users_db.Next() {
+		var id, id_message int
+		err := users_db.Scan(&id, &id_message)
+		if err != nil {
+			return err
+		}
+		response = append(response, UserHeadline{
+			Id: id,
+			Id_message: id_message,
+			Quotes: nil,
+		})
+	}
+
+	// возьми список юзеров
+	for i, user := range response {
+		rows, err := db.Query(
+			`with filter as
+				(select * from users_quotes where id_user = $1)
+			select quotes.name, quotes.cost
+				from filter 
+				left join quotes 
+					on filter.id_quote = quotes.id`,
+			&user.Id,		
+		)
+		if err != nil {
+			return err
+		}
+	
+		res := make([]Quote, 0)
+		for rows.Next() {
+			var cost float32
+			var name string
+			err = rows.Scan(&name, &cost) 
+			if err != nil {
+				return err
+			}
+			res = append(res, Quote{name, cost})
+		}
+		response[i].Quotes = res
+		rows.Close()
+	}
+
+	// для каждого собери данные
+	return c.JSON(http.StatusOK, response)
+	// верни данные
+}
+
+func add_headline(c echo.Context) error {
+	headline := new(Headline)
+
+	if err := c.Bind(headline); err != nil {
+		return c.JSON(http.StatusBadRequest, map[string]string{
+			"error": "Invalid JSON format",
+		})
+	}
+	db, err := sql.Open("sqlite3", "my.sql")
+	if err != nil {
+		return err
+	}
+	defer db.Close()
+
+	ans, err := db.Query(
+		`select id from quotes where id = $1`, 
+		&headline.Id, 
+	)
+	if err != nil {
+		return err
+	}
+	defer ans.Close()
+	
+	if !ans.Next() {
+		_, err = db.Exec(
+			`Insert into quotes values($1, $2, $3)`, 
+			&headline.Id, 
+			"",
+			0,
+		)
+		if err != nil {
+			return err
+		}
+	}
+	_, err = db.Exec(
+		`Insert into users_quotes values($1, $2)`, 
+		&headline.Id, 
+		&headline.Id_user,
+	)
+	if err != nil {
+		return err
+	}
+
+	ch, ok := c.Get("signalChan").(chan struct{})
+	if !ok {
+		return c.String(http.StatusInternalServerError, "Error: chan not found")
+	}
+	select {
+		case ch <- struct{}{}:
+		default:		
+	}
+	
+	return c.String(http.StatusOK, "new headline added")
+}
+
+
+// unused
+func get_quotes(c echo.Context) error {
+	user := new(UserId)
+
+	if err := c.Bind(user); err != nil {
+		return c.JSON(http.StatusBadRequest, map[string]string{
+			"error": "Invalid JSON format",
+		})
+	}
+
+	db, err := sql.Open("sqlite3", "my.sql")
+	if err != nil {
+		return err
+	}
+	defer db.Close()
+
+	rows, err := db.Query(
+		`with filter as
+			(select * from users_quotes where id_user = $1)
+		select quotes.name, quotes.cost
+			from filter 
+			left join quotes 
+				on filter.id_quote = quotes.id`,
+		&user.Id,		
+	)
+	if err != nil {
+		return err
+	}
+
+	res := make([]Quote, 0)
+	for rows.Next() {
+		var cost float32
+		var name string
+		err = rows.Scan(&name, &cost) 
+		if err != nil {
+            return err
+        }
+		res = append(res, Quote{name, cost})
+	}
+	rows.Close()
+
+	rows, err = db.Query(
+		`select id_message from users where id = $1`, 
+		&user.Id,
+	)
+	if err != nil {
+		return err
+	}
+	var id int
+	if rows.Next() {
+		err := rows.Scan(&id)
+		if err != nil {
+			return err
+		}
+	}
+	rows.Close()
+	response := UserHeadline{
+		Quotes: res,
+		Id: user.Id,
+		Id_message: id,
+	}
+	return c.JSON(http.StatusOK, response)
+
+	
+}
 
 func add_user(c echo.Context) error {
 	user := new(User)
@@ -57,14 +259,14 @@ func add_user(c echo.Context) error {
 		return err
 	}
 
-	return c.String(http.StatusOK, "new user was create")
+	return c.String(http.StatusOK, "new user created")
 }
 
 func hello(c echo.Context) error {
 	return c.String(http.StatusOK, "hello, world!")
 }
 
-func show_users(c echo.Context) error {
+func get_users(c echo.Context) error {
 	db, err := sql.Open("sqlite3", "my.sql")
 	if err != nil {
 		return err
@@ -100,14 +302,38 @@ func ConnectToDB() error {
 
 	_, err = db.Exec(`
 	CREATE TABLE if not exists users (
-		id INTEGER Primiry Key,
+		id INTEGER Primary Key,
 		name varchar(50),
 		id_message INTEGER
 	)`)
 	if err != nil {
         return err
     }
+	_, err = db.Exec(`
+	CREATE TABLE if not exists users_quotes (
+		id_quote varchar(12),
+		id_user INTEGER
+	)`)
+	if err != nil {
+        return err
+    }
+
+	_, err = db.Exec(`
+	CREATE TABLE if not exists quotes (
+		id varchar(12),
+		name varchar(50),
+		cost REAL
+	)`)
+	if err != nil {
+        return err
+    }
+
 	return nil
+}
+
+type Headline struct {
+	Id string `json:"id"`
+	Id_user int `json:"id_user"`
 }
 
 type User struct {
@@ -115,3 +341,23 @@ type User struct {
 	Name string `json:"name"`
 	Id_message int `json:"id_message"`
 }
+
+type UserId struct {
+	Id int `json:"id"`
+}
+
+type UserHeadline struct{
+	Id int `json:"id"`
+	Id_message int `json:"id_message"`
+	Quotes []Quote `json:"quotes"`
+}
+
+type Quote struct {
+	Name string `json:"name"`
+	Cost float32 `json:"cost"`
+}
+
+
+
+
+
--- internal/aggregator/aggregator.go
+++ internal/aggregator/aggregator.go
@@ -1,6 +1,108 @@
 package aggregator
 
+import (
+	"context"
+	//"fmt"
+	"log"
+	"os/signal"
+	"syscall"
+	"time"
+	"database/sql"
+
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+
+	"github.com/russianinvestments/invest-api-go-sdk/investgo"
+	_ "github.com/russianinvestments/invest-api-go-sdk/proto"
+)
 
 func Run() {
-	
+	config, err := investgo.LoadConfig("config.yaml")
+	if err != nil {
+		log.Fatalf("config loading error %v", err.Error())
+	}
+
+	ctx, cancel := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM, syscall.SIGKILL)
+	defer cancel()
+	// сдк использует для внутреннего логирования investgo.Logger
+	// для примера передадим uber.zap
+	zapConfig := zap.NewDevelopmentConfig()
+	zapConfig.EncoderConfig.EncodeTime = zapcore.TimeEncoderOfLayout(time.DateTime)
+	zapConfig.EncoderConfig.TimeKey = "time"
+	l, err := zapConfig.Build()
+	logger := l.Sugar()
+	defer func() {
+		err := logger.Sync()
+		if err != nil {
+			log.Print(err.Error())
+		}
+	}()
+	if err != nil {
+		log.Fatalf("logger creating error %v", err)
+	}
+	// создаем клиента для investAPI, он позволяет создавать нужные сервисы и уже
+	// через них вызывать нужные методы
+
+	// если вы хотите передать опции для создания соединения grpc.ClientConnOption, то передайте их в NewClient
+	// client, err := investgo.NewClient(ctx, config, logger, grpc.WithDefaultCallOptions(grpc.MaxCallRecvMsgSize(1e6)))
+	client, err := investgo.NewClient(ctx, config, logger)
+	if err != nil {
+		logger.Fatalf("client creating error %v", err.Error())
+	}
+	defer func() {
+		logger.Infof("closing client connection")
+		err := client.Stop()
+		if err != nil {
+			logger.Errorf("client shutdown error %v", err.Error())
+		}
+	}()
+
+	db, err := sql.Open("sqlite3", "my.sql")
+	if err != nil {
+		log.Print(err.Error())
+		return
+	}
+	defer db.Close()
+
+	rows, err := db.Query(`SELECT id FROM quotes`)
+	if err != nil {
+		log.Print(err.Error())
+		return 
+	}
+	defer rows.Close()
+
+	instruments := make([]string, 0)
+	for rows.Next() {
+		var s string
+		err = rows.Scan(&s,) 
+		if err != nil {
+			log.Print(err.Error())
+            return 
+        }
+		instruments = append(instruments, s)
+	}
+
+	// создаем клиента для сервиса маркетдаты
+	MarketDataService := client.NewMarketDataServiceClient()
+	//instruments := []string{"BBG004730N88", "BBG00475KKY8", "BBG004RVFCY3"}
+
+	lastPriceResp, err := MarketDataService.GetLastPrices(instruments)
+	if err != nil {
+		logger.Error(err.Error())
+		return 
+	} 
+	lp := lastPriceResp.GetLastPrices()
+	/*for i, price := range lp {
+		fmt.Printf("last price number %v = %v\n", i, price.GetPrice().ToFloat())
+	}*/
+	for i, v := range instruments {
+		_, err = db.Exec(`Update quotes SET cost = $1 WHERE id = $2`, 
+			lp[i].GetPrice().ToFloat(),
+			v,
+		)
+		if err != nil {
+			log.Print(err.Error())
+			return
+		}
+	} 
 }
